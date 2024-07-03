80e7a8d2e061f0e34ad9e7d25782f558115094d7
Gabriela Cruz <76226475+gabiicruzp@users.noreply.github.com>
2024-07-03 19:29:12 -0300

    Update and rename 8a. Manipulação de Strings e datas.sql to 08a. Manipulação de Strings e datas.sql

--- 08a. Manipulação de Strings e datas.sql
+++ 08a. Manipulação de Strings e datas.sql
@@ -2,10 +2,10 @@
 
 -- LEN e DATALENGTH: 
 SELECT LEN('Gabriela Cruz  ') AS 'Len'
-SELECT DATALENGTH('Gabriela Cruz  ') AS 'DataLength' -- datalength diferencia do Len por considerar espaços adicionais
+SELECT DATALENGTH('Gabriela Cruz  ') AS 'DataLength' -- datalength diferencia do Len por considerar espaÃ§os adicionais
 
 
--- CONCAT: permite juntar mais de um texto em uma única palavra
+-- CONCAT: permite juntar mais de um texto em uma Ãºnica palavra
 SELECT
 	FirstName AS 'Nome',
 	LastName AS 'Sobrenome',
@@ -22,14 +22,14 @@ SELECT RIGHT('Product0101001', 7)
 
 SELECT 
 	ProductName AS 'Produto', 
-	UnitPrice AS 'Preço',
+	UnitPrice AS 'PreÃ§o',
 	LEFT(StyleName, 7) AS 'Cod1',
 	RIGHT(StyleName, 7) AS 'Cod2'
 FROM DimProduct
 
 
 -- REPLACE: substitui um texto por outro
-SELECT REPLACE('O Excel é o melhor', 'Excel', 'SQL') 
+SELECT REPLACE('O Excel Ã© o melhor', 'Excel', 'SQL') 
 
 SELECT TOP(5) FROM DimCustomer
 SELECT
@@ -45,7 +45,7 @@ SELECT TRANSLATE ('10.241/444.124k23/1', './k', '---')
 SELECT TRANSLATE ('10.241/444.124k23/1', './k', 'xxx')
 SELECT TRANSLATE ('ABCD-490123', 'ABCD', 'WXYZ')
 
-SELECT STUFF ('Python Impressionador', 1, 6, 'Excel') -- o segundo argumento é a posicao que começa o texto, o terceiro argumento é quantos caracteres tem o texto, e o quarto é qual o texto que vai entrar
+SELECT STUFF ('Python Impressionador', 1, 6, 'Excel') -- o segundo argumento Ã© a posicao que comeÃ§a o texto, o terceiro argumento Ã© quantos caracteres tem o texto, e o quarto Ã© qual o texto que vai entrar
 
 SELECT
 	STUFF ('MT98-Moto G', 1, 2, 'CEL'),
@@ -66,13 +66,13 @@ FROM
 -- FORMAT
 --1. Formatar geral
 SELECT FORMAT(5123, 'G') --G de geral
---2. Formatar número
-SELECT FORMAT(5123, 'N') --N de número
+--2. Formatar nÃºmero
+SELECT FORMAT(5123, 'N') --N de nÃºmero
 --3. Formatar moeda
 SELECT FORMAT(5123, 'C') --C de currency
 --4. Formatar data
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'dd/MM/yyyy')
-SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'dd/MMMM/yyyy', 'en-US') -- O 3° argumento é p add idioma
+SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'dd/MMMM/yyyy', 'en-US') -- O 3Â° argumento Ã© p add idioma
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'dd')
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'ddd')
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'dddd')
@@ -81,26 +81,26 @@ SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'MMM')
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'MMMM')
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'yy')
 SELECT FORMAT(CAST('25/06/2024' AS DATETIME), 'yyyy')
---5. Formatação personalizada
+--5. FormataÃ§Ã£o personalizada
 SELECT FORMAT(1234567, '##-##-###')
 
 
 -- CHARINDEX e SUBSTRING
---1. Descubra a posição em que começa o sobrenome da Raquel 
-SELECT CHARINDEX('M', 'Raquel Moreno') -- a letra M esta na posição 8
-SELECT CHARINDEX('Moreno', 'Raquel Moreno') -- a letra M esta na posição 8
+--1. Descubra a posiÃ§Ã£o em que comeÃ§a o sobrenome da Raquel 
+SELECT CHARINDEX('M', 'Raquel Moreno') -- a letra M esta na posiÃ§Ã£o 8
+SELECT CHARINDEX('Moreno', 'Raquel Moreno') -- a letra M esta na posiÃ§Ã£o 8
 --2. Extraia o sobrenome da Raquel 
-SELECT SUBSTRING('Raquel Moreno', 8, 6) -- o 2° argumento é onde queremos extrair e p 3° até onde queremos
---3. Combine as 2 funções e extraia de forma automática
---podemos usar o charindex para selecionar algo em comum, no caso o espaço entre o nome e sobrenome, e então add '+1' pq queremos APÓS o espaço. E o argumento final (100) é a qnt max de caracteres que queremos
+SELECT SUBSTRING('Raquel Moreno', 8, 6) -- o 2Â° argumento Ã© onde queremos extrair e p 3Â° atÃ© onde queremos
+--3. Combine as 2 funÃ§Ãµes e extraia de forma automÃ¡tica
+--podemos usar o charindex para selecionar algo em comum, no caso o espaÃ§o entre o nome e sobrenome, e entÃ£o add '+1' pq queremos APÃ“S o espaÃ§o. E o argumento final (100) Ã© a qnt max de caracteres que queremos
 SELECT SUBSTRING('Bernardo Cavalcanti', CHARINDEX(' ', 'Bernardo Cavalcanti') +1 , 100)
 SELECT SUBSTRING('Raquel Moreno', CHARINDEX(' ', 'Raquel Moreno') +1 , 100)
---4. Automatize a questão interior
+--4. Automatize a questÃ£o interior
 DECLARE @varnome VARCHAR (100) = 'Bernardo Cavalcanti'
 SELECT SUBSTRING(@varnome, CHARINDEX(' ', @varnome) +1 , 100)
 
 
--- TRIM, LTRIM, RTRIM: retira espaços adicionais no texto 
+-- TRIM, LTRIM, RTRIM: retira espaÃ§os adicionais no texto 
 DECLARE @codigo VARCHAR(50) = '  ACB123   '
 SELECT 
 	TRIM(@codigo) AS 'Trim',
@@ -118,7 +118,7 @@ SELECT
 DECLARE @data DATETIME = '25/06/2024'
 SELECT
 	DAY(@data) AS 'Data',
-	MONTH(@data) AS 'Mês',
+	MONTH(@data) AS 'MÃªs',
 	YEAR(@data) AS 'Ano'
 
 
@@ -154,7 +154,7 @@ SELECT
 	SQL_VARIANT_PROPERTY(DATEPART(DAY, @data), 'BaseType')
 
 
---DATEADD e DATEDIFF: permite add/subtrair uma qnt em relação a uma data e a outra calcula a diferença entre duas datas
+--DATEADD e DATEDIFF: permite add/subtrair uma qnt em relaÃ§Ã£o a uma data e a outra calcula a diferenÃ§a entre duas datas
 DECLARE @data1 DATETIME = '10/07/2020'
 DECLARE @data2 DATETIME = '05/03/2020'
 DECLARE @data3 DATETIME = '14/11/2021'
